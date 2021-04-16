288a7cdb563f44f29cc4a64e0532aad0b63bed04
Francesca Litterio <francesca@litterio.net>
2021-04-16 19:48:29 -0400

    Delete DSA-LinkedList directory

--- DSA-LinkedList/sorting.js
+++
@@ -1,7 +0,0 @@
-//when item1 < item 2, change pointer so that next > previous
-//but what is the best way do do this sort! want most efficient
-
-function sort(list){
-    let curr = list.head;
-    let next = list.head.next;
-}
