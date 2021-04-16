8224aa1ea90c089f172f3236da2d9a3dfffb47c3
Francesca Litterio <francesca@litterio.net>
2021-04-16 19:48:08 -0400

    Delete DSA-Big-O-main directory

--- DSA-Big-O-main/iv-factorial.js
+++
@@ -1,9 +0,0 @@
-const factorial = (n) => {
-    let num = 1
-    for (let i =1; i <= n; i++){
-        num = num* i
-    }
-    return num;
-}
-
-console.log(factorial(5));
--- DSA-Big-O-main/iv-split.js
+++
@@ -1,9 +0,0 @@
-const split = (string, splitter) => {
-    let output = []
-
-    for(let i = 0; i <= string.length; i++){
-        if(string[i] === splitter){
-            
-        }
-    }
-}
